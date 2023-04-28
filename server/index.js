cd94ceb931771e2ec99bf53bcdce16b5c46c8b46
Yadav106 <sahil.jp.yadav@gmail.com>
2023-04-29 01:54:40 +0530

    cleanup v2

--- server/index.js
+++ server/index.js
@@ -180,67 +180,6 @@ app.put('/update_purchase', async (req, res) => {
     }
 });
 
-// app.put('/delete_order', async (req, res) => {
-//     try {
-//         const body = req.body;
-//         console.log(body);
-//         const userId = body['id'];
-//         const user = await collection.findOne({ id: userId });
-//         if (!user) {
-//             res.status(404).json({ message: 'User not found' });
-//             return;
-//         }
-//         result = await collection.updateOne({ id: userId }, { $pull : { "products": {'id': body['productId']} } });
-//         console.log(result);
-//         res.json({ message: 'Order deleted' });
-//     } catch(err) {
-//         console.error(err);
-//         res.status(500).json({ message: 'Server Error' });
-//     }
-// });
-
-// app.put('/add_order', async (req, res) => {
-//     try {
-//         const body = req.body;
-//         console.log(body);
-//         const userId = body['id'];
-//         const user = await collection.findOne({ id: userId });
-//         if (!user) {
-//             res.status(404).json({ message: 'User not found' });
-//             return;
-//         }
-//         const order = {
-//             id: body['productId'],
-//             productName: body['productName'],
-//             price: body['price'],
-//             rating : body['rating']
-//         }
-//         const result = await collection.updateOne({id: userId}, { $push : {"products" : order } })
-//         res.json({ message: 'Order added'});
-//     } catch(err) {
-//         console.error(err);
-//         res.status(500).json({ message: 'Server Error' });
-//     }
-// });
-
-// app.post('/add_user', async (req, res) => {
-//     try {
-//         const body = req.body;
-//         console.log(body);
-//         const userId = body['id'];
-//         const user = await collection.findOne({ id: userId });
-//         if (user) {
-//             res.status(404).json({ message: 'User already exists' });
-//             return;
-//         }
-//         const result = await collection.insertOne(body);
-//         res.json({ message: 'User added'});
-//     } catch(err) {
-//         console.error(err);
-//         res.status(500).json({ message: 'Server Error' });
-//     }
-// });
-
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
